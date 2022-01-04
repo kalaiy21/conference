4d31d0997191d10fc207de025f68189ba19fc244
M-Arvind <68342779+M-Arvind@users.noreply.github.com>
2022-01-04 23:55:49 +0530

    Add Image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
+import skcetLogo from '../Resources/skcetLogo.png';
 
 function navbar() {
     return (
         <div className="font-poppins bg-[#272727] w-[15%] h-screen flex items-center justify-center fixed top-0 left-0">
             <div className="absolute top-10">
-                <img className="w-5 h-5" src="../Resources/skcetLogo.png" alt="logo" />
+                <img className="w-15 h-10" src={skcetLogo}  alt="logo" />
             </div>
             <div className="text-white flex flex-col text-center space-y-6">
                 <a className="p-[2px] transition-all" href="#">Home</a>
