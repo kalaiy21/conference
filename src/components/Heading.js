1c0789395364666ac81a06773e7cef6284af8c0a
M-Arvind <68342779+M-Arvind@users.noreply.github.com>
2022-01-05 00:11:43 +0530

    change in heading component Done..

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Heading() {
     return(
-        <div className="flex justify-center mt-[3rem]">
-            <h1 className="text-center text-3xl Heading">International Conference on innovations in Robotics, Intelligent Automation and Control</h1>
+        <div className="flex flex-col text-center justify-center items-center my-[1rem]">
+            <p className="text-xs font-semibold opacity-[50%]">Sri Krishna College of Engineering and Technology</p>
+            <p className="text-xs font-semibold mb-[1rem] opacity-[50%]">Department of Mechatronics Engineering</p>
+            <h1 className="text-[1.4rem] w-[50rem] uppercase">International Conference on innovations in Robotics, Intelligent Automation and Control</h1>
         </div>
     );
 }
