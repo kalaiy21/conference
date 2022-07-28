eca4b1c3842ddcd012448a4bd0a5672bacac8e36
kalaiy21 <kalaiytz2132002@gmail.com>
2022-07-28 14:14:31 +0530

    navbar responsive

--- src/components/General/Navbar.js
+++ src/components/General/Navbar.js
@@ -8,11 +8,11 @@ function navbar(props) {
     const tabs = ["Home", "About SKCET", "Call for Paper", "Committee", "Keynote", "Important Dates", "Submission", "Publication", "Registration", "Downloads", "Venue", "Contact"];
 
     return (
-        <div className="font-poppins bg-[#206dac] rounded-r-2xl w-[15%] h-screen flex items-center space-y-16 justify-center fixed top-0 left-0">
+        <div className="font-poppins bg-[#206dac] rounded-r-2xl w-[15%] h-screen flex items-center justify-center fixed top-0 left-0">
             <div className="absolute top-10">
                 <img className="h-10 w-15" src={skcetLogo}  alt="logo" />
             </div>
-            <div className="flex flex-col space-y-6 font-semibold text-center text-white tracking-[1px]">
+            <div className="mt-[4rem] flex flex-col xl:space-y-6 lg:space-y-3 md:space-y-2 md:text-[.8rem] xl:text-[1rem] font-semibold text-center text-white tracking-[1px]">
 
                 {tabs.map((tab) => props.Underline===tab ? <a className="p-[2px] transition-all underline underline-offset-8" key={tab} href="#" onClick={onTabSelected}>{tab}</a> : <a className="p-[2px] transition-all" key={tab}href="#" onClick={onTabSelected}>{tab}</a>)}
 
