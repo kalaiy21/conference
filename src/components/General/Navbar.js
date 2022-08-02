9f473b73d7e454cb59f37620cb9e8cfab25a0209
kalaiy21 <kalaiytz2132002@gmail.com>
2022-08-02 21:58:53 +0530

    nav responsive solved

--- src/components/General/Navbar.js
+++ src/components/General/Navbar.js
@@ -8,11 +8,11 @@ function navbar(props) {
     const tabs = ["Home", "About SKCET", "Call for Paper", "Committee", "Keynote", "Important Dates", "Submission", "Publication", "Registration", "Downloads", "Venue", "Contact"];
 
     return (
-        <div className="font-poppins bg-[#206dac] rounded-r-2xl w-[15%] h-screen flex items-center justify-center fixed top-0 left-0">
-            <div className="absolute top-10">
+        <div className="font-poppins bg-[#206dac] rounded-r-2xl w-[15%] h-screen flex flex-col snap-y items-center justify-center fixed top-0 left-0">
+            <div className="">
                 <img className="h-10 w-15" src={skcetLogo}  alt="logo" />
             </div>
-            <div className="mt-[4rem] flex flex-col xl:space-y-6 lg:space-y-3 md:space-y-2 md:text-[.8rem] xl:text-[1rem] font-semibold text-center text-white tracking-[1px]">
+            <div className="mt-[3rem] h-[75%] content-between justify-between flex flex-col md:text-[.8rem] xl:text-[1rem] font-semibold text-center text-white tracking-[1px]">
 
                 {tabs.map((tab) => props.Underline===tab ? <a className="p-[2px] transition-all underline underline-offset-8" key={tab} href="#" onClick={onTabSelected}>{tab}</a> : <a className="p-[2px] transition-all" key={tab}href="#" onClick={onTabSelected}>{tab}</a>)}
 
@@ -20,5 +20,6 @@ function navbar(props) {
         </div>  
     );
 }
+// lg:space-y-3 xl:space-y-6 md:space-y-2
 
 export default navbar;
