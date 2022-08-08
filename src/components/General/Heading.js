77d986fb65a4d4ff441625c629ce107a07e0cf5f
hari2001-hash <19eumt035@skcet.ac.in>
2022-08-08 13:25:35 +0530

    Edited header

--- src/components/General/Heading.js
+++ src/components/General/Heading.js
@@ -5,7 +5,9 @@ function Heading() {
         <div className="flex flex-col bg-skcet w-full text-center justify-center items-center bg-no-repeat bg-cover py-[3rem] mb-[3rem] font-Roboto font-medium">
             <div className="bg-[#4b92cc]/[.55] text-white py-[1rem] rounded-xl">
                 <p className="text-xl font-semibold tracking-[2px] leading-[1.2rem]">Sri Krishna College of Engineering and Technology</p>
-                <p className="text-xl font-semibold mb-[1rem] tracking-[2px] leading-[2.3rem]">Department of Mechatronics Engineering</p>
+                <p className="text-xl font-semibold mb-[0.7rem] tracking-[2px] leading-[2.3rem]">Department of Mechatronics Engineering</p>
+                <p className="text-l font-semibold mb-[0.7rem] tracking-[1px] leading-[1rem]">in association with</p>
+                <p className="font-semibold tracking-[2px] leading-[2rem]">KYUNGPOOK NATIONAL UNIVERSITY, South Korea</p>
                 <h1 className="text-[1.4rem] mb-[1rem] w-[50rem] uppercase leading-[2.3rem] tracking-[1px]">International Conference on innovations in Robotics, Intelligent Automation and Control (ICIRIAC 2022)</h1>
                 <p className=" font-semibold tracking-[2px] leading-[2.3rem]">14/10/2022 - Coimbatore, Tamilnadu, India</p>
             </div>
